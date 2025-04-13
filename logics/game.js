98b8bff7bc35f4585281fbb562918d2bc05f5060
Douglas Senzu <senzdigitals.inc23@gmail.com>
2025-04-13 09:49:16 +0000

    Created switchPlayer method to switch between player 1 and 2, and report turn method to show whose turn is it to play

--- logics/game.js
+++ logics/game.js
@@ -6,3 +6,18 @@ let countPlayerOneMoves = 0;
 let countPlayerTwoMoves = 0;
 let playerOneWins = 0;
 let playerTwoWins = 0;
+
+let switchPlayer = (player) => {
+    playerTurn = player;
+
+    return playerTurn;
+}
+
+
+let reportTurn = () => {
+    if (playerTurn == 1) {
+        return "Player 1's turn";
+    }else if (playerTurn == 2) {
+        return "Player 2's turn";
+    }
+}
